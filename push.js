c90e2bf5ebe4f15f82816d1b47e5fc109595ca73
unknown <xdxaverdrabik@gmail.com>
2020-02-06 12:10:33 +0100

    fix

--- push.js
+++ push.js
@@ -1,12 +1,26 @@
-var push = require('web-push');
+var push = require("web-push");
 
 let vapidKeys = {
-    publicKey: 'BCohg_Q7urt4q8suITza2p1XZPFEENCIneO5PhbQ4ezj1xVstG9IW1xzwoHkStqSw0S4Ag7T3rXPUJpvu2wZGrY',
-    privateKey: 'Uxqx6QjVzmMPMArJDIR2LYxXa_wiVroIt9pLD_fICdE'
+  publicKey:
+    "BCohg_Q7urt4q8suITza2p1XZPFEENCIneO5PhbQ4ezj1xVstG9IW1xzwoHkStqSw0S4Ag7T3rXPUJpvu2wZGrY",
+  privateKey: "Uxqx6QjVzmMPMArJDIR2LYxXa_wiVroIt9pLD_fICdE"
 };
 
-push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey);
+push.setVapidDetails(
+  "mailto:test@code.co.uk",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
 
-let sub = {};
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/exIvHybOm0U:APA91bFpa6T5LQdHcsn1kNpx737-H1SfRK6NbpnR209RI21NfsccLYr2UKseodCTc3w3eequlbnwJzAsODuaV5g6ibUFKH-28jzuEov_e3tm3oaLDhX_sbGNIvC0uwmggRTX1eiZkXAG",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BEjeuF05ArU1S11S_zz7IgT9CR4HxRlr0Jd1S-hVY7JdRYxtRco2GwqHd_zWS5Zto_pi76pepXc1UaOenO2xOHs",
+    auth: "_L-X6011IsbVcnULQC0RGw"
+  }
+};
 
-push.sendNotification(sub, 'message');
+push.sendNotification(sub, "message");
